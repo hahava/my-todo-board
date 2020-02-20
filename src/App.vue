afe65a53ae45d690a3dd45638b87a7e357706e2b
hahava <hahava5696@gmail.com>
2020-02-20 19:24:34 +0900

    Add board container

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>MY Todo Board</h3>
+    <br>
+    <br>
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <BoardItem board-status="TODO"
+                     board-title="hello TODO board"
+                     board-content="hello board content"/>
+          <BoardItem board-status="TODO"
+                     board-title="hello TODO board"
+                     board-content="hello board content"/>
+        </div>
+        <div class="col-4">
+          <BoardItem board-status="DOING"
+                     board-title="hello DONE board"
+                     board-content="hello board content"/>
+        </div>
+        <div class="col-4">
+          <BoardItem board-status="DONE"
+                     board-title="hello  DOINGboard"
+                     board-content="hello board content"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BoardItem  from "./components/BoardItem"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BoardItem
   }
 }
 </script>
