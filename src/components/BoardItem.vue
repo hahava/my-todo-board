9ed7e72aba1519ad0073c5a9a60ef60b2f136e3a
hahava <hahava@naver.com>
2020-09-16 22:49:39 +0900

    Refactor Boarditem.vue using ts

--- src/components/BoardItem.vue
+++ src/components/BoardItem.vue
@@ -1,53 +1,53 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <h5 class="card-title text-left">{{item.title}}</h5>
-      <p class="card-text text-left">{{item.content}}</p>
+      <h5 class="card-title text-left">{{ item.title }}</h5>
+      <p class="card-text text-left">{{ item.content }}</p>
       <button class="btn btn-sm float-right"
               v-bind:class="btnStyle"
               v-on:click="changeNextStep">
-        {{item.status}}
+        {{ item.status }}
       </button>
     </div>
   </div>
 </template>
 
-<script>
-    import 'bootstrap/dist/css/bootstrap.css'
-    import 'bootstrap-vue/dist/bootstrap-vue.css'
-    import {EVENT_TYPE, EventBus} from "../main";
-    import {STATUS_TYPE} from "../main";
+<script lang="ts">
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import {EVENT_TYPE, EventBus, Item, STATUS_TYPE} from "@/main";
+import {Component, Prop, Vue} from "vue-property-decorator";
 
-    export default {
-        name: "BoardItem",
-        props: ["item"],
-        methods: {
-            changeNextStep() {
-                EventBus.$emit(EVENT_TYPE.CHANGE_STATUS, this.item);
-            }
-        },
-        computed: {
-            btnStyle() {
-                if (this.item.status === STATUS_TYPE.TODO) {
-                    return "btn-secondary"
-                }
-                if (this.item.status === STATUS_TYPE.DOING) {
-                    return "btn-primary"
-                }
-                return "btn-success disabled"
-            }
-        }
+@Component
+export default class BoardItem extends Vue {
+
+  @Prop()
+  item?: Item
+
+  changeNextStep() {
+    EventBus.$emit(EVENT_TYPE.CHANGE_STATUS, this.item);
+  }
+
+  get btnStyle() {
+    if (this.item?.status === STATUS_TYPE.TODO) {
+      return "btn-secondary"
     }
+    if (this.item?.status === STATUS_TYPE.DOING) {
+      return "btn-primary"
+    }
+    return "btn-success disabled"
+  }
+}
 </script>
 
 <style scoped>
-  div {
-    margin-top: 10px;
-    margin-bottom: 10px;
-  }
+div {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
 
-  button {
-    position: inherit;
-    right: 0px;
-  }
+button {
+  position: inherit;
+  right: 0px;
+}
 </style>
